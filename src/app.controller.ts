51898c2cff2b141a70ff227e8431c9be9abdf66f
Jaime Cantú <69063285+jaimecantu@users.noreply.github.com>
2022-05-12 09:44:54 -0500

    prep for deploy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,77 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  readAll() {
-    return this.appService.readAll();
-  }
-
-  @Get(":name")
-  readFile(@Param("name") name: string) {
-    return this.appService.readFile(name);
-  }
-
-  @Get("remove/tags")
-  removeTags() {
-    return this.appService.removeTags();
-  }
-
-  @Get("process/words")
-  words() {
-    return this.appService.words();
-  }
-
-  @Get("process/consolidate")
-  consolidated() {
-    return this.appService.consolidate();
-  }
-
-  @Get("process/tokenize")
-  tokenize() {
-    return this.appService.tokenize();
-  }
-
-  @Get("process/counter")
-  counter() {
-    return this.appService.counter();
-  }
-
-  @Get("process/posting")
-  posting() {
-    return this.appService.posting();
-  }
-
-  @Get("process/hash")
-  hash() {
-    return this.appService.hashtable();
-  }
-
-  @Get("process/stoplist")
-  stoplist() {
-    return this.appService.stoplist();
-  }
-
-  @Get("process/weight")
-  weightTokens() {
-    return this.appService.weightTokens();
-  }
-
-  @Get("process/documents")
-  documents() {
-    return this.appService.documents();
-  }
-
-  @Get("process/index")
-  createIndex() {
-    return this.appService.createIndex();
-  }
-
-  @Get("process/search/:word")
-  search(@Param("word") word: string) {
-    return this.appService.search(word);
-  }
-
-  @Get("process/optimized/:word")
+  @Get("/:word")
   optimizedSearch(@Param("word") word: string) {
     return this.appService.optimizedSearch(word);
   }
